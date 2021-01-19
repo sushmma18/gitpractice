847abe8ee9dc2a01aa46bf67496cf1b735ef15f0
sushmma18 <sushmmanair01@gmail.com>
2021-01-19 22:20:53 +0530

    user commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        Sting name="Rithu";
+        Sting name="User";
+        String address="Address";
         System.out.println(name);
+	System.out.println(address);
+
+
+
     }
 }
